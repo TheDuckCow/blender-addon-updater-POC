5f49f8f05794d0da68b05dd3caf27cace1ce1cfa
TheDuckCow <theduckcow@live.com>
2016-01-26 02:07:17 -0500

    pre tag 1 release for pulling info from website

--- blender_addon_updater.py
+++ blender_addon_updater.py
@@ -31,7 +31,7 @@ Layout planning
 - Addon preferences panel for how to check/notify
 
 Todo
-- Convert to zip folder structure to have local directory?
+- implement popup UI with prop_popup and a UIList
 - Use the github API or just flat out do website scraping?
 https://developer.github.com/v3/
 
@@ -43,17 +43,6 @@ import os
 import urllib.request
 from html.parser import HTMLParser
 from bpy.app.handlers import persistent
-
-bl_info = {
-	"name": "Addon & Blender Updater (Proof of Concept)",
-	"description": "Check and update for new addon and blender versions",
-	"author": "Patrick W. Crawford",
-	"version": (0, 0, 1),
-	"blender": (2, 76, 0),
-	"wiki_url": "https://github.com/TheDuckCow/blender-addon-updater/",
-	"location": "Info bar",
-	"warning": "In development",
-	"category": "System"}
 
 
 # -----------------------------------------------------------------------------
@@ -88,6 +77,10 @@ class updater_check_addon_updates(bpy.types.Operator):
 		# parse to see if update available
 		# bring up menu
 
+		global ADDON_UPDATE_LIST
+		global ADDON_UPDATE
+		ADDON_UPDATE_LIST.clear()
+
 		link_list = self.list_updates(context)
 		for addon in link_list:
 			# first parse for tag releases, append "release/" to the link
@@ -95,15 +88,24 @@ class updater_check_addon_updates(bpy.types.Operator):
 			update_status = self.check_for_update(addon)
 			if update_status['available']==True:
 				ADDON_UPDATE_LIST.append(update_status)
+				print("added "+update_status["name"])
 		if len(ADDON_UPDATE_LIST) > 0: ADDON_UPDATE = True
+
+		# menu popup to show
+		if self.ui == True:
+			bpy.ops.wm.updater_addon_popup('INVOKE_DEFAULT')
+		else:
+			print("Update READY! Show user in another way..")
 
 		return {'FINISHED'}
 
 
 	def list_updates(self,context):
 		print("grabbing the list of links to check for updates")
-		# hard coded for now
+		# hard coded for now, but this would grab from a webserver
+		# or scrape a website will a listing of all addons (and source links)
 
+		print("Hard coded: checking for updates to the updater addon? 2meta4me")
 		return ['https://github.com/TheDuckCow/blender-addon-updater/']
 
 
@@ -123,11 +125,12 @@ class updater_check_addon_updates(bpy.types.Operator):
 		download_link = addon_link+"tags/"	
 		print("Download page:",download_link)
 
-		update_status = {'available':False,
-					'link':'https://github.com/TheDuckCow/blender-addon-updater/',
+		update_status = {'available':True,
+					'link':'https://github.com/TheDuckCow/blender-addon-updater/tags',
 					'type':'py',
 					'name':'Addon & Blender Updater'}
 		return update_status
+
 
 
 class updater_addon_run_update(bpy.types.Operator):
@@ -139,8 +142,8 @@ class updater_addon_run_update(bpy.types.Operator):
 	# though, this gives a redo-last item.. not desirable,
 	# should be an enum if anything.
 	addon_name = bpy.props.StringProperty(
-		description="Addon to update")
-
+		description="Addon to update",
+		default="")
 
 	def execute(self,context):
 		
@@ -152,6 +155,7 @@ class updater_addon_run_update(bpy.types.Operator):
 			if addon["name"] != self.addon_name:continue
 			addon_info = addon
 			break
+		if addon_info=={}: return # didn't find the addon match
 
 		# addon_info is now the addon we want to update
 
@@ -159,6 +163,7 @@ class updater_addon_run_update(bpy.types.Operator):
 		# download the repository in whole
 		# as it could be more than a .py
 		# should download in the addon's local folder directory, a temp folder
+
 
 		return {'FINISHED'}
 
@@ -168,7 +173,66 @@ class updater_addon_run_update(bpy.types.Operator):
 		# for this hard coded example, we just know it needs to be
 		# the most recent tag link
 
+		content = urllib.request.urlopen(link,timeout=100).read().decode('utf-8')
+
+		print("#### HTML CONTENT")
+		print(content)
+		print("#### END HTML CONTENT")
+
 		return 
+
+
+
+class updater_addon_popup(bpy.types.Operator):
+	"""Popup menu to display current updates available"""
+	bl_idname = "wm.updater_addon_popup"
+	bl_label = "Updater popup"
+	bl_description = "Popup menu to display current updates available"
+
+	def invoke(self, context, event):
+		return context.window_manager.invoke_popup(self) # can force width
+
+	def draw(self, context):
+		layout = self.layout
+		print("popup")
+
+		# case where there are no updates
+		if len(ADDON_UPDATE_LIST)==0:
+			col = layout.column()
+			col.label("No addon updates found")
+			col.label("Click OK to close popup")
+			return
+
+		# otherwise, list the updates
+		row = layout.row()
+		row.label("Addons with available updates")
+		row = layout.row()
+		col1 = row.column()
+		col2 = row.column()
+		for addon in ADDON_UPDATE_LIST:
+
+			col1.operator("wm.updater_addon_run_update",
+				text="Update x addon".format(x=addon["name"])
+				).addon_name = addon["name"]
+			col2.operator("wm.url_open", text="Website").url = addon["link"]
+
+		# but with this UI method, pressing a button does not
+		# dismiss the popup, though full control over UI and is draggable
+
+		# alternative would be check boxes or a UI list where user
+		# checks all the ones they want updated
+		
+
+
+	def execute(self,context):
+
+		# based on elections above, decide which ones to install
+		# this function doesn't actually run anything,
+		# but this format forces a floating "OK" window
+		print("ran popup exec")
+		return {'FINISHED'}
+
+
 
 # hack to auto-run updater when opening blender
 @persistent
@@ -195,24 +259,59 @@ def load_handler(scene):
 # http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/System/Check_for_updates
 # but is based on website scraping and already is broken
 
+class updater_check_blender_update(bpy.types.Operator):
+	"""Check for new blender update"""
+	bl_idname = "wm.updater_check_blender_update"
+	bl_label = "Check blender update"
+	bl_description = "Check for new blender update"
+
+	def execute(self,context):
+		
+		self.report({'ERROR'}, "Not implemented yet")
+		return {'CANCELLED'}
+
+		return {'FINISHED'}
 
 # -----------------------------------------------------------------------------
 # MENU/PREFERENCES DEFINITIONS
 # -----------------------------------------------------------------------------
 
-def addon_header_menu(self, context):
-	if ADDON_UPDATE == True:
-		self.layout.operator(
-			updater_addon_run_update.bl_idname,
-			text="Updates available",
-			icon='ERROR'
-		)
+def update_header_menu_APPEND(self, context):
+	
+	if ADDON_UPDATE == True or BLENDER_UPDATE == True:
+		self.layout.menu(update_menu.bl_idname, icon='ERROR',\
+				text="Update available")
 	else:
-		self.layout.operator(
-			updater_check_addon_updates.bl_idname,
-			text="Check for update"
-		)
+		self.layout.menu(update_menu.bl_idname)
 
+
+class update_menu(bpy.types.Menu):
+	bl_label = "Check updates"
+	bl_idname = "updater_check_menu"
+	bl_description = "Check for blender and addon updates"
+
+	def draw(self, context):
+		layout = self.layout
+		if ADDON_UPDATE==True:
+			layout.operator(updater_check_addon_updates.bl_idname,icon="ERROR",\
+					text="Addon updates available")
+		else:
+			layout.operator(updater_check_addon_updates.bl_idname)
+		
+		if BLENDER_UPDATE==True:
+			layout.operator(updater_check_blender_update.bl_idname,icon='ERROR',\
+					text="Blender update available")
+		else:
+			layout.operator(updater_check_blender_update.bl_idname)
+
+
+def debug_addon_update(self, context):
+	global ADDON_UPDATE
+	ADDON_UPDATE = self.debug_addon
+
+def debug_blender_update(self, context):
+	global BLENDER_UPDATE
+	BLENDER_UPDATE = self.debug_blender
 
 class updater_preferences(bpy.types.AddonPreferences):
 	bl_idname = __package__
@@ -235,6 +334,18 @@ class updater_preferences(bpy.types.AddonPreferences):
 			('custom', "Custom", "Set custom update", 4),
 		],
 		default = 'week')
+	
+	debug_addon = bpy.props.BoolProperty(
+		name="Pretend addon updates ready",
+		default=False,
+		update=debug_addon_update
+	)
+
+	debug_blender = bpy.props.BoolProperty(
+		name="Pretend blender update ready",
+		default=False,
+		update=debug_blender_update
+	)
 
 	# setting for how the notification appears?
 
@@ -243,9 +354,18 @@ class updater_preferences(bpy.types.AddonPreferences):
 		layout = self.layout
 		row = layout.row()
 		row.prop(self,"auto_check_update")
-		row.prop(self,"check_frequency")
 		row = layout.row()
-		row.label("")
+		row.enabled = self.auto_check_update
+		row.prop(self,"check_frequency")
+		if self.check_frequency=='custom':
+			row = layout.row()
+			row.label("eventually custom text box for days/months/etc")
+		col = layout.column()
+		col.label("Debugging")
+		box = col.box()
+		box.prop(self,"debug_addon")
+		box.prop(self,"debug_blender")
+		box.label("Addon:{x}, Blender:{y}".format(x=ADDON_UPDATE,y=BLENDER_UPDATE))
 
 
 # -----------------------------------------------------------------------------
@@ -254,16 +374,16 @@ class updater_preferences(bpy.types.AddonPreferences):
 
 
 def register():
-	bpy.utils.register_module(__name__)
-	bpy.types.USERPREF_HT_header.append(addon_header_menu)
+	bpy.types.USERPREF_HT_header.append(update_header_menu_APPEND)
+	#bpy.types.USERPREF_HT_header.append("updater_check_menu")
 
 	# the "hack" to run the update check on opening blender, run it here
 	# add handler that then auto-removes itself to run with context
 	bpy.app.handlers.scene_update_pre.append(load_handler)
 		
 def unregister():
-	bpy.utils.unregister_module(__name__)
-	bpy.types.USERPREF_HT_header.remove(addon_header_menu)
+	bpy.types.USERPREF_HT_header.remove(update_header_menu_APPEND)
+	# bpy.types.USERPREF_HT_header.remove("updater_check_menu")
 
 if __name__ == "__main__":
 	register()
